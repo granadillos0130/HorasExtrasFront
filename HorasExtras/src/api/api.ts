03857523c465335c626402af5d0ddfd47541847a
Lenovo-pc <Lenovo-pc@DESKTOP-L2TP2PQ>
2025-07-31 08:45:15 -0500

    subir lo de ngrok

--- HorasExtras/src/api/api.ts
+++ HorasExtras/src/api/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://localhost:7042/api",
+  baseURL: import.meta.env.VITE_API_URL || "https://localhost:7042/api",
   headers: {
     "Content-Type": "application/json"
   }
