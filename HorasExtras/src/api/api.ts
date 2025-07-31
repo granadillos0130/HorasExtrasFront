58012d577b493ebc907ac5a44d797d1cac0d2431
Lenovo-pc <granadossantiago2006@gmail.com>
2025-07-31 09:28:57 -0500

    autorizacion  ngrok

--- HorasExtras/src/api/api.ts
+++ HorasExtras/src/api/api.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 export const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL || "https://localhost:7042/api",
   headers: {
-    "Content-Type": "application/json"
+    "Content-Type": "application/json",
+    "ngrok-skip-browser-warning": "true" // 🔧 Esto bypasea la advertencia
   }
-  
 });
