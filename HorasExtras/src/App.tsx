d9902df56ff47d3e1e82e6d3eef1bc2cae29f88b
Lenovo-pc <granadossantiago2006@gmail.com>
2025-09-26 11:39:59 -0500

    Borrar cache 2025-09-26v2

--- HorasExtras/src/App.tsx
+++ HorasExtras/src/App.tsx
@@ -27,7 +27,7 @@ import CompensadoForm from "./components/compensado/CompensadoForm";
 // CACHE MANAGER
 const useCacheManager = () => {
   useEffect(() => {
-    const CURRENT_VERSION = '2025-09-26v1'; // Cambia esto con cada versión nueva
+    const CURRENT_VERSION = '2025-09-26v2'; // Cambia esto con cada versión nueva
     const lastVersion = localStorage.getItem('app_version');
     
     if (lastVersion !== CURRENT_VERSION) {
