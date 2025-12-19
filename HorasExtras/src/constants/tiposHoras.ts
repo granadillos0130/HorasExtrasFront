37adb847a2c018011cfd357bf560519865374852
Lenovo-pc <granadossantiago2006@gmail.com>
2025-12-19 09:59:44 -0500

    feat: agregar tipos de horas de desplazamiento en la configuración

--- HorasExtras/src/constants/tiposHoras.ts
+++ HorasExtras/src/constants/tiposHoras.ts
@@ -35,5 +35,26 @@ export const TIPOS_HORAS_CONFIG = {
     icono: '🌜',
     color: '#8b5cf6',
     descripcion: 'Horas trabajadas domingos de noche (x2.1)'
+  },
+   desplazamientonormal: {
+    key: 'desplazamientonormal',
+    nombre: 'Desplazamiento Normal',
+    icono: '🚗',
+    color: '#06b6d4',
+    descripcion: 'Horas de desplazamiento dentro de jornada (x1.0)'
+  },
+  desplazamientoextradiurno: {
+    key: 'desplazamientoextradiurno',
+    nombre: 'Desplazamiento Extra Diurno',
+    icono: '🚙',
+    color: '#f97316',
+    descripcion: 'Horas de desplazamiento extra diurno (x1.25)'
+  },
+  desplazamientoextranocturno: {
+    key: 'desplazamientoextranocturno',
+    nombre: 'Desplazamiento Extra Nocturno',
+    icono: '🚕',
+    color: '#7c3aed',
+    descripcion: 'Horas de desplazamiento extra nocturno (x1.75)'
   }
 };
